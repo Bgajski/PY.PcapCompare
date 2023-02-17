f2cde33d9df276f6d78a1e3bf4f2acb67c84634d
Benjamin Gajski <47593223+Bgajski@users.noreply.github.com>
2023-02-17 11:15:35 +0100

    Update pcapCompare.py

--- pcapCompare.py
+++ pcapCompare.py
@@ -3,41 +3,32 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pyshark
 
-# scrap pcap
-pcapScrap1 = pyshark.FileCapture('static\images\file1.pcap', only_summaries=True)
-protocolList1 = []
-for packet in pcapScrap1:
-    line = str(packet)
-    formattedLine = line.split(" ")
-    print(formattedLine)
-    protocolList1.append(formattedLine[4])  
-    pcapData1 = collections.Counter(protocolList1)
-print(protocolList1)
+def read_pcap(file_path):
+    protocol_list = []
+    pcap_scrap = pyshark.FileCapture(file_path, only_summaries=True)
+    for packet in pcap_scrap:
+        line = str(packet)
+        formatted_line = line.split(" ")
+        protocol_list.append(formatted_line[4])
+    return collections.Counter(protocol_list), protocol_list
 
-pcapScrap2 = pyshark.FileCapture('static\images\file2.pcap', only_summaries=True)
-protocolList2 = []
-for packet in pcapScrap2:
-    line = str(packet)
-    formattedLine = line.split(" ")
-    print(formattedLine)
-    protocolList2.append(formattedLine[4]) 
-    pcapData2 = collections.Counter(protocolList2)
-print(protocolList2)
+pcap_data1, protocol_list1 = read_pcap('static/images/file1.pcap')
+pcap_data2, protocol_list2 = read_pcap('static/images/file2.pcap')
 
 # Show protocol list frequency
 plt.style.use('ggplot')
 
-x_pos = np.arange(len(list(pcapData1.keys())))
-y_pos = np.arange(len(list(pcapData2.keys())))
+x_pos = np.arange(len(list(pcap_data1.keys())))
+y_pos = np.arange(len(list(pcap_data2.keys())))
 
-plt.bar(x_pos, list(pcapData1.values()), align='center', alpha=0.5, color=['b', 'g', 'r', 'c', 'm'])
-plt.xticks(x_pos, list(pcapData1.keys()))
+plt.bar(x_pos, list(pcap_data1.values()), align='center', alpha=0.5, color=['b', 'g', 'r', 'c', 'm'])
+plt.xticks(x_pos, list(pcap_data1.keys()))
 
-plt.bar(y_pos, list(pcapData2.values()), align='center', alpha=0.5, color=['black'])
-plt.xticks(y_pos, list(pcapData2.keys()))
+plt.bar(y_pos, list(pcap_data2.values()), align='center', alpha=0.5, color=['black'])
+plt.xticks(y_pos, list(pcap_data2.keys()))
 
 plt.title("Packets comparison")
 plt.ylabel("Frequency")
 plt.xlabel("Protocol Name")
-plt.savefig("static\graph\Pcap.png")
+plt.savefig("static/graph/Pcap.png")
 plt.show()
