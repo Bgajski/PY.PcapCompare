54c01cad3402e0640e87a340f5bd4cb72dd09fa1
Benjamin Gajski <47593223+Bgajski@users.noreply.github.com>
2023-02-17 15:40:33 +0100

    Update pcapCompare.py

--- pcapCompare.py
+++ pcapCompare.py
@@ -1,19 +1,24 @@
 import collections
+import matplotlib
 import numpy as np
 import matplotlib.pyplot as plt
 import pyshark
 
+matplotlib.use('TkAgg')
+
+
 def read_pcap(file_path):
     protocol_list = []
-    pcap_scrap = pyshark.FileCapture(file_path, only_summaries=True)
-    for packet in pcap_scrap:
-        line = str(packet)
-        formatted_line = line.split(" ")
-        protocol_list.append(formatted_line[4])
-    return collections.Counter(protocol_list), protocol_list
+    with pyshark.FileCapture(file_path, only_summaries=True) as pcap_scrap:
+        for packet in pcap_scrap:
+            line = str(packet)
+            formatted_line = line.split(" ")
+            protocol_list.append(formatted_line[4])
+        return collections.Counter(protocol_list), protocol_list
 
-pcap_data1, protocol_list1 = read_pcap('static/images/file1.pcap')
-pcap_data2, protocol_list2 = read_pcap('static/images/file2.pcap')
+
+pcap_data1, protocol_list1 = read_pcap('static/pcap/file1.pcap')
+pcap_data2, protocol_list2 = read_pcap('static/pcap/file1.pcap')
 
 # Show protocol list frequency
 plt.style.use('ggplot')
@@ -24,7 +29,7 @@ y_pos = np.arange(len(list(pcap_data2.keys())))
 plt.bar(x_pos, list(pcap_data1.values()), align='center', alpha=0.5, color=['b', 'g', 'r', 'c', 'm'])
 plt.xticks(x_pos, list(pcap_data1.keys()))
 
-plt.bar(y_pos, list(pcap_data2.values()), align='center', alpha=0.5, color=['black'])
+plt.bar(y_pos, list(pcap_data2.values()), align='center', alpha=0.5, color=['pink'])
 plt.xticks(y_pos, list(pcap_data2.keys()))
 
 plt.title("Packets comparison")
